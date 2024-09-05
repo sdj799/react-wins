b7a42895e6ab3897f13adca2897468a954b8dc65
mya413 <lsm940413@gmail.com>
2024-09-05 11:10:14 +0900

    add: layout 타입 추가

---
+++ src/types/layout.d.ts
@@ -0,0 +1,13 @@
+export type NavEventType = {
+  $isShowNav: boolean;
+};
+
+export type NavType = {
+  id: number;
+  title: string;
+  desc: string[] | string;
+};
+
+export type TabType = {
+  tab: string;
+};
