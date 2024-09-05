82e099f3d412a3334ae35aa4514432fcad664026
mya413 <lsm940413@gmail.com>
2024-09-05 12:27:15 +0900

    feat: 페이지별 router 추가

---
+++ src/router/index.tsx
@@ -0,0 +1,82 @@
+import { createBrowserRouter } from "react-router-dom";
+import BasicLayout from "layouts/BasicLayout";
+import CommonLayout from "layouts/CommonLayout";
+import Home from "@pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    element: <BasicLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    element: <CommonLayout />,
+    children: [
+      // 컴포넌트 router에 맞게 순서대로 추가
+      {
+        path: "/ktwiz",
+        element: "구단소개 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/ktwiz/history",
+        element: "구단연혁 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/wizpark",
+        element: "구장소개 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/wizpark/guide",
+        element: "구장안내도 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/wizpark/location",
+        element: "찾아오기 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/game",
+        element: "정규리그 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/game/score",
+        element: "박스스코어 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/game/ranking",
+        element: "순위 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/game/watchPoint",
+        element: "관전포인트 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/player",
+        element: "코칭스텝 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/player/pitcher",
+        element: "투수 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/player/catcher",
+        element: "타자 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/player/cheer",
+        element: "응원단 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/media",
+        element: "wiz소식 component", // 컴포넌트 삽입
+      },
+      {
+        path: "/media/wizpress",
+        element: "wiz보도자료 component", // 컴포넌트 삽입
+      },
+    ],
+  },
+]);
