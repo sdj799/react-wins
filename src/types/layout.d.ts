e7f3366b6bf48240b3540dc949240328208decf9
mya413 <lsm940413@gmail.com>
2024-09-10 10:59:11 +0900

    style: layout type 추가

--- src/types/layout.d.ts
+++ src/types/layout.d.ts
@@ -2,10 +2,27 @@ export type NavEventType = {
   $isShowNav: boolean;
 };
 
+export type NavDescType = {
+  id: number;
+  desc: string;
+};
+
+export type NavPathType = {
+  id: number;
+  path: string;
+};
+
+export type NavTabType = {
+  id: number;
+  tab: string;
+};
+
 export type NavType = {
   id: number;
   title: string;
-  desc: string[] | string;
+  desc: NavDescType[] | string;
+  path: NavPathType[] | string;
+  tab?: NavTabType[];
 };
 
 export type TabType = {
