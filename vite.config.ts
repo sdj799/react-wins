5f5ce876ceec532d9bb70a3f2adb5a81a53ab558
dongjun <sdj799@naver.com>
2024-09-15 14:18:32 +0900

    fix: .env파일 전역상수로 구현

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      __BASE_URL__: JSON.stringify(env.BASE_URL),
+      __KAKAOMAP_APP_KEY__: JSON.stringify(env.KAKAOMAP_APP_KEY),
+    },
+  };
 });
