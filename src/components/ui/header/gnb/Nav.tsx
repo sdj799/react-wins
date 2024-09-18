075482c111828a0fe8b2a3d3d64b07b4d9f5ca5c
Sonjieun <sonjieun2661@gmail.com>
2024-09-18 23:41:37 +0900

    refactor: 네브바 wizpark 수정

--- src/components/ui/header/gnb/Nav.tsx
+++ src/components/ui/header/gnb/Nav.tsx
@@ -6,6 +6,8 @@ import TopMenu from "./TopMenu";
 const MainNavStyle = styled.nav<{ $isShowNav: boolean }>`
   display: ${(props) => props.$isShowNav && "grid"};
   grid-template-columns: ${(props) => props.$isShowNav && "repeat(8, 1fr)"};
+  text-align: center;
+  width: 850px;
 `;
 
 const Nav = ({ $isShowNav }: NavEventType) => {
