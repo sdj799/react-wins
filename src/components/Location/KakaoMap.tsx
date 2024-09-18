270794fbbeb89c7eb9772d8f538598ef4d8bb2fc
dongjun <sdj799@naver.com>
2024-09-18 22:33:15 +0900

    design: 카카오맵 마커 디자인 추가

--- src/components/Location/KakaoMap.tsx
+++ src/components/Location/KakaoMap.tsx
@@ -1,8 +1,22 @@
+import img from "@assets/icons/historyIcon.png";
 import { TKakaoMapp } from "@customTypes/kakaomap";
 import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
 import iksanMap from "../../assets/images/iksan_map.png";
 import locationMap from "../../assets/images/map_img.png";
+
+const Overlay = styled.div`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 147px;
+  height: 28px;
+  color: #333;
+  text-align: center;
+  font-size: 18px;
+`;
 
 declare const __KAKAOMAP_APP_KEY__: string;
 const KakaoMap = ({ lat, lng, location }: TKakaoMapp) => {
@@ -18,8 +32,16 @@ const KakaoMap = ({ lat, lng, location }: TKakaoMapp) => {
   return (
     <>
       <Map center={{ lat: lat, lng: lng }} style={{ width: "100%", height: "662px" }} level={2}>
-        <MapMarker position={{ lat: lat, lng: lng }}>
-          <div style={{ color: "#000" }}>{location}</div>
+        <MapMarker
+          position={{ lat: lat, lng: lng }}
+          image={{
+            src: img,
+            size: {
+              width: 45,
+              height: 45,
+            },
+          }}>
+          <Overlay>{location}</Overlay>
         </MapMarker>
       </Map>
     </>
