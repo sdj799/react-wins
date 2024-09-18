b0a5fc0b74c251e6f75585dfe5f44e7ac78e8a94
Sonjieun <sonjieun2661@gmail.com>
2024-09-18 19:33:49 +0900

    feat:페이지네이션 연결

--- src/components/Media/PaginationButtons.tsx
+++ src/components/Media/PaginationButtons.tsx
@@ -1,4 +1,11 @@
-import { MdKeyboardArrowRight, MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { useEffect, useState } from "react";
+import {
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
 const PagenationWrapper = styled.nav`
@@ -7,39 +14,94 @@ const PagenationWrapper = styled.nav`
   text-align: center;
 `;
 
-const PageButton = styled.button`
+const PageButton = styled.button<{ $isSelected?: boolean }>`
   width: 26px;
   height: 26px;
-  background-color: #fff;
+  background-color: ${({ $isSelected }) => ($isSelected ? "#e60013" : "#fff")};
+  color: ${({ $isSelected }) => ($isSelected ? "#fff" : "#999")};
   border: 1px solid #c3c3c3;
   border-radius: 2px;
   margin: 0 2px;
   padding: 0px;
   font-size: 12px;
   line-height: 26px;
-  color: #999;
   vertical-align: middle;
 `;
 
 const PaginationButtons = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [pageGroup, setPageGroup] = useState(0);
+  const [selectedNum, setSelectedNum] = useState(1);
+  const PAGE_COUNT = 10;
+  const MAX_PAGE = 116;
+  const maxGroup = Math.floor(MAX_PAGE / PAGE_COUNT);
+
+  useEffect(() => {
+    const currentPage = searchParams.get("search.page");
+    setPageGroup(Math.floor(Number(currentPage) / PAGE_COUNT));
+    setSelectedNum(Number(currentPage));
+  }, []);
+  useEffect(() => {
+    setSearchParams({ "search.page": `${selectedNum}` });
+  }, [selectedNum]);
+
+  const handlePageChange = (pageNum: number) => {
+    setSelectedNum(pageNum);
+  };
+  const handlePageGroupPrev = () => {
+    if (pageGroup === 0) return;
+    setPageGroup((prev) => prev - 1);
+    setSelectedNum((prev) => prev - PAGE_COUNT);
+  };
+  const handlePageGroupNext = () => {
+    if (pageGroup === maxGroup) return;
+    setPageGroup((prev) => prev + 1);
+    setSelectedNum((prev) => prev + PAGE_COUNT);
+  };
+  const handlePageFirst = () => {
+    setPageGroup(0);
+    setSelectedNum(1);
+  };
+  const handlePageLast = () => {
+    setPageGroup(maxGroup);
+    setSelectedNum(MAX_PAGE);
+  };
+
   return (
     <PagenationWrapper>
-      <PageButton>1</PageButton>
-      <PageButton>2</PageButton>
-      <PageButton>3</PageButton>
-      <PageButton>4</PageButton>
-      <PageButton>5</PageButton>
-      <PageButton>6</PageButton>
-      <PageButton>7</PageButton>
-      <PageButton>8</PageButton>
-      <PageButton>9</PageButton>
-      <PageButton>10</PageButton>
-      <PageButton>
-        <MdKeyboardArrowRight>다음</MdKeyboardArrowRight>
-      </PageButton>
-      <PageButton>
-        <MdKeyboardDoubleArrowRight>마지막</MdKeyboardDoubleArrowRight>
-      </PageButton>
+      {selectedNum !== 1 && (
+        <PageButton onClick={handlePageFirst}>
+          <MdKeyboardDoubleArrowLeft>처음</MdKeyboardDoubleArrowLeft>
+        </PageButton>
+      )}
+      {pageGroup !== 0 && (
+        <PageButton onClick={handlePageGroupPrev}>
+          <MdKeyboardArrowLeft>이전</MdKeyboardArrowLeft>
+        </PageButton>
+      )}
+      {Array.from({ length: 10 }, (_, index) => {
+        const pageNumber = pageGroup * PAGE_COUNT + index + 1;
+        if (pageNumber <= MAX_PAGE) {
+          return (
+            <PageButton
+              key={index}
+              $isSelected={pageNumber === selectedNum}
+              onClick={() => handlePageChange(pageNumber)}>
+              {pageNumber}
+            </PageButton>
+          );
+        }
+      })}
+      {pageGroup !== maxGroup && (
+        <PageButton onClick={handlePageGroupNext}>
+          <MdKeyboardArrowRight>다음</MdKeyboardArrowRight>
+        </PageButton>
+      )}
+      {selectedNum !== MAX_PAGE && (
+        <PageButton onClick={handlePageLast}>
+          <MdKeyboardDoubleArrowRight>마지막</MdKeyboardDoubleArrowRight>
+        </PageButton>
+      )}
     </PagenationWrapper>
   );
 };
