98cc0d40956a50ab42b6de656ac89a77ba6bd0e0
Sonjieun <sonjieun2661@gmail.com>
2024-09-19 16:32:49 +0900

    design :중요구현 외 페이지 구현

---
+++ src/pages/Store.tsx
@@ -0,0 +1,13 @@
+import { StyledImage } from "@styles/ImageStyle.style";
+
+const Store = () => {
+  return (
+    <>
+      <StyledImage
+        src="https://wizzap.ktwiz.co.kr/files/resource/2023/03/28/20230328173650.914-513d0b606ab1.png"
+        alt="층별 편의시설 안내"
+      />
+    </>
+  );
+};
+export default Store;
