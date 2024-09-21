fd427736e607ee1e99213d131dad4cdefa81212e
Parkjieun <0224h@naver.com>
2024-09-22 02:13:11 +0900

    feat: 관중현황 페이지 관중기록 테이블 API 연동

--- src/components/Audience/AudienceRecord.tsx
+++ src/components/Audience/AudienceRecord.tsx
@@ -1,27 +1,10 @@
 import ArticleTitle from "@components/common/ArticleTitle";
+import { Tcrowd } from "@customTypes/Crowd";
 import { StyledArticle } from "@styles/Ranking.style";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-interface TeamData {
-  rank: number;
-  team: string;
-  games: number;
-  totalAttendance: number;
-  avgAttendance: number;
-}
+import { api } from "../../api/api.ts";
 
-// 임의 데이터
-const teamData: TeamData[] = [
-  { rank: 1, team: "LG", games: 68, totalAttendance: 1281420, avgAttendance: 18844 },
-  { rank: 2, team: "삼성", games: 70, totalAttendance: 1275022, avgAttendance: 18215 },
-  { rank: 3, team: "두산", games: 69, totalAttendance: 1257215, avgAttendance: 18221 },
-  { rank: 4, team: "KIA", games: 69, totalAttendance: 1177249, avgAttendance: 17062 },
-  { rank: 5, team: "롯데", games: 68, totalAttendance: 1168597, avgAttendance: 17185 },
-  { rank: 6, team: "SSG", games: 69, totalAttendance: 1100862, avgAttendance: 15955 },
-  { rank: 7, team: "키움", games: 72, totalAttendance: 792350, avgAttendance: 11005 },
-  { rank: 8, team: "KT", games: 66, totalAttendance: 768260, avgAttendance: 11640 },
-  { rank: 9, team: "한화", games: 66, totalAttendance: 745797, avgAttendance: 11300 },
-  { rank: 10, team: "NC", games: 69, totalAttendance: 700742, avgAttendance: 10156 },
-];
 const TableWrapper = styled.div`
   max-width: 1200px;
   margin: 0 auto;
@@ -67,6 +50,15 @@ const HighlightRow = styled.tr`
   }
 `;
 const AudienceRecord = () => {
+  const [crowdData, setCrowdData] = useState<Tcrowd[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await api("game/rank/crowd?gyear=2024");
+      setCrowdData(data.list);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <StyledArticle>
@@ -83,22 +75,22 @@ const AudienceRecord = () => {
               </tr>
             </TableHeader>
             <TableBody>
-              {teamData.map((team) =>
-                team.team === "KT" ? (
-                  <HighlightRow key={team.rank}>
-                    <td>{team.rank}</td>
-                    <td>{team.team}</td>
-                    <td>{team.games}</td>
-                    <td>{team.totalAttendance.toLocaleString()}</td>
-                    <td>{team.avgAttendance.toLocaleString()}</td>
+              {crowdData.map((team, index) =>
+                team.teamName === "KT" ? (
+                  <HighlightRow key={index}>
+                    <td>{index + 1}</td>
+                    <td>{team.teamName}</td>
+                    <td>{team.game}</td>
+                    <td>{team.crowd.toLocaleString()}</td>
+                    <td>{team.crowd.toLocaleString()}</td>
                   </HighlightRow>
                 ) : (
-                  <tr key={team.rank}>
-                    <td>{team.rank}</td>
-                    <td>{team.team}</td>
-                    <td>{team.games}</td>
-                    <td>{team.totalAttendance.toLocaleString()}</td>
-                    <td>{team.avgAttendance.toLocaleString()}</td>
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{team.teamName}</td>
+                    <td>{team.game}</td>
+                    <td>{team.crowd.toLocaleString()}</td>
+                    <td>{team.crowd.toLocaleString()}</td>
                   </tr>
                 )
               )}
