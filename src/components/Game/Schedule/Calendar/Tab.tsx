82a5239bba84e69419ce38e7bd0649fbcebdff39
mya413 <lsm940413@gmail.com>
2024-09-25 00:19:08 +0900

    feat: tab메뉴 구현

--- src/components/Game/Schedule/Calendar/Tab.tsx
+++ src/components/Game/Schedule/Calendar/Tab.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+interface TabProps {
+  activeTab: string;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+}
+
 const TabStyle = styled.ul`
   width: 100%;
   max-width: 240px;
@@ -15,19 +20,24 @@ const TabStyle = styled.ul`
     font-weight: 300;
     font-size: 14px;
     text-align: center;
-  }
+    cursor: pointer;
 
-  & > li:first-child {
-    background-color: #ec0a0b;
-    color: #fff;
+    &.active {
+      background-color: #ec0a0b;
+      color: #fff;
+    }
   }
 `;
 
-const Tab = () => {
+const Tab = ({ activeTab, setActiveTab }: TabProps) => {
+  const tabList = ["kt wiz 경기", "전체 리그"];
   return (
     <TabStyle>
-      <li>kt wiz 경기</li>
-      <li>전체 리그</li>
+      {tabList.map((tab, index) => (
+        <li key={index} className={activeTab === tab ? "active" : ""} onClick={() => setActiveTab(tab)}>
+          {tab}
+        </li>
+      ))}
     </TabStyle>
   );
 };
